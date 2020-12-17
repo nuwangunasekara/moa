c721bca4ae26876df90a67d85a92d40b9b70a988
Nuwan Gunasekara <ng198002@gmail.com>
2020-12-18 11:19:22 +1300

    scrip to to manage conda build env

---
+++ moa/src/main/scripts/reinit_conda.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+conda_env_path=$1
+conda_yml_file=$2
+#scikit_multiflow_path=$3
+
+if [ $# -l 2 ]; then
+  echo "$#"
+  echo "$0 <conda_env_path> <conda_yml_file>"
+  exit 1
+else
+  echo  "$@"
+fi
+
+
+eval "$(conda shell.bash hook)"
+conda init bash
+
+
+re_init_conda_env ()
+{
+  echo "Removing conda env $conda_env_path"
+  conda remove  --prefix $conda_env_path --all
+
+  echo "Creating conda env $conda_env_path from config $conda_yml_file"
+  conda env create --prefix $conda_env_path --file $conda_yml_file
+
+  echo "Updating conda env $conda_env_path from config $conda_yml_file"
+  conda env update --prefix $conda_env_path --file $conda_yml_file  --prune
+
+  conda activate $conda_env_path
+  conda env list
+
+  #pip3 install -e $scikit_multiflow_path
+}
+
+re_init_conda_env
