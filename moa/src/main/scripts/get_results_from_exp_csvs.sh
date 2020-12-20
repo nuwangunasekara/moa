6a56bd279a47f387d96ca7febce1de9071754786
Nuwan Gunasekara <ng198002@gmail.com>
2020-12-20 23:55:44 +1300

    Update log script

--- moa/src/main/scripts/get_results_from_exp_csvs.sh
+++ moa/src/main/scripts/get_results_from_exp_csvs.sh
@@ -1,3 +1,4 @@
 RESULTS_DIR='/Scratch/ng98/JavaSetup1/resultsNN/Exp2/Results/'
+DATASET_DIR='/Scratch/ng98/datasets/NEW/unzipped/'
 
-find  $RESULTS_DIR -iname '*.txt' |sed 's/ /\\ /g' |xargs awk '{print $0}'|grep  'Learner' -v |sed "s/ArffFileStream -f ${RESULTS_DIR//\//\\\/}" |sed 's/.arff//g' | awk -F ',' '{printf "%30s, %30s, %40s, %40s, %40s\n", $1,$2,$5,$6,$8}'
+find  $RESULTS_DIR -iname '*.txt' |sed 's/ /\\ /g' | xargs awk '{print $0}' |grep  'Learner' -v |sed "s#ArffFileStream -f ${DATASET_DIR//\//\\\/}##" |sed 's/.arff//g' | awk -F ',' '{printf "%30s, %40s, %40s, %40s, %40s\n", $1,$2,$5,$6,$8}'
