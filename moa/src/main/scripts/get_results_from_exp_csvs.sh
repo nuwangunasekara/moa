7424e9e2e282022c01d2659def232aa61f41c469
Nuwan Gunasekara <ng198002@gmail.com>
2020-12-26 10:27:12 +1300

    Add support to print headers

--- moa/src/main/scripts/get_results_from_exp_csvs.sh
+++ moa/src/main/scripts/get_results_from_exp_csvs.sh
@@ -1,4 +1,13 @@
-RESULTS_DIR='/Scratch/ng98/JavaSetup1/resultsNN/Exp2/Results/'
-DATASET_DIR='/Scratch/ng98/datasets/NEW/unzipped/'
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 <dataset_dir> <results_csv_dir>"
+  echo "e.g:   $0 /Scratch/ng98/datasets/NEW/unzipped/ /Scratch/ng98/JavaSetup1/resultsNN/Exp8/"
+  exit 1
+fi
 
-find  $RESULTS_DIR -iname '*.txt' |sed 's/ /\\ /g' | xargs awk '{print $0}' |grep  'Learner' -v |sed "s#ArffFileStream -f ${DATASET_DIR//\//\\\/}##" |sed 's/.arff//g' | awk -F ',' '{printf "%30s, %40s, %40s, %40s, %40s\n", $1,$2,$5,$6,$8}'
+DATASET_DIR=$1"/"
+RESULTS_DIR=$2"/"
+
+s_pattern='*.csv'
+p_pattern='{printf "%30s, %40s, %40s, %40s, %40s\n", $1,$2,$5,$6,$8}'
+find  $RESULTS_DIR -iname "$s_pattern" | grep -m 1 '.csv' |xargs head -n 1 |awk -F ',' "$p_pattern"
+find  $RESULTS_DIR -iname "$s_pattern" |sed 's/ /\\ /g' | xargs awk '{print $0}' |grep  'Learner' -v |sed "s#ArffFileStream -f ${DATASET_DIR//\//\\\/}##" |sed 's/.arff//g' | awk -F ',' "$p_pattern"
