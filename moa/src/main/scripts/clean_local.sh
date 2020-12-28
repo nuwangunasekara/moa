4c173062a8c03656fbe3a9da8a266555669966e4
Nuwan Gunasekara <ng198002@gmail.com>
2020-12-29 07:55:20 +1300

    do not remove apache/logging/log4j

--- moa/src/main/scripts/clean_local.sh
+++ moa/src/main/scripts/clean_local.sh
@@ -1,6 +1,6 @@
 rm -r ~/.m2/repository/ai
 rm -r ~/.m2/repository/org/slf4j
-rm -r ~/.m2/repository/org/apache/logging/log4j
+#rm -r ~/.m2/repository/org/apache/logging/log4j
 rm -r ~/.m2/repository/com/google/code/gson 
 rm -r ~/.m2/repository/net/java/dev/jna 
 rm -r ~/.m2/repository/org/apache/commons
