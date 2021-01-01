c4e2193e928e36b4445c54ac8db32fc4bf0b09d8
Nuwan Gunasekara <ng198002@gmail.com>
2021-01-01 22:15:13 +1300

    add support to exclude search

--- moa/src/main/scripts/get_results_from_exp_csvs.sh
+++ moa/src/main/scripts/get_results_from_exp_csvs.sh
@@ -1,6 +1,6 @@
 if [ $# -lt 2 ]; then
-  echo "Usage: $0 <dataset_dir> <results_csv_dir> <file_search_pattern>"
-  echo "e.g:   $0 /Scratch/ng98/datasets/NEW/unzipped/ /Scratch/ng98/JavaSetup1/resultsNN/Exp8/ '*.txt'"
+  echo "Usage: $0 <dataset_dir> <results_csv_dir> <file_search_pattern> <exclude_file_search_pattern>"
+  echo "e.g:   $0 /Scratch/ng98/datasets/NEW/unzipped/ /Scratch/ng98/JavaSetup1/resultsNN/Exp8/ '*.txt' 'NN_loss'"
   exit 1
 fi
 
@@ -11,7 +11,13 @@ if [ $# -gt 2 ]; then
 else
   s_pattern='*.csv'
 fi
+
+if [ $# -gt 3 ]; then
+  exclude_s_pattern=$4
+else
+  exclude_s_pattern=' '
+fi
 s_pattern='*.csv'
 p_pattern='{printf "%30s, %40s, %40s, %40s, %40s\n", $1,$2,$5,$6,$8}'
-find  $RESULTS_DIR -iname "$s_pattern" | grep -m 1 '.csv' |xargs head -n 1 |awk -F ',' "$p_pattern"
-find  $RESULTS_DIR -iname "$s_pattern" |sed 's/ /\\ /g' | xargs awk '{print $0}' |grep  'Learner' -v |sed "s#ArffFileStream -f ##g" |sed "s#${DATASET_DIR}##g" |sed "s#/\/#\\\#g" |sed 's/.arff//g' | awk -F ',' "$p_pattern"
+find  $RESULTS_DIR -iname "$s_pattern" | grep -v "$exclude_s_pattern" | grep -m 1 '.csv' |xargs head -n 1 |awk -F ',' "$p_pattern"
+find  $RESULTS_DIR -iname "$s_pattern" | grep -v "$exclude_s_pattern" | sed 's/ /\\ /g'  | xargs awk '{print $0}' |grep  'Learner' -v |sed "s#ArffFileStream -f ##g" |sed "s#${DATASET_DIR}##g" |sed "s#/\/#\\\#g" |sed 's/.arff//g' | awk -F ',' "$p_pattern"
