053a7ef1a46898270dcffcf3eec9d16265538c5a
Nuwan Gunasekara <ng198002@gmail.com>
2021-01-03 10:24:23 +1300

    Fix an error in the script

--- moa/src/main/scripts/get_results_from_exp_csvs.sh
+++ moa/src/main/scripts/get_results_from_exp_csvs.sh
@@ -17,7 +17,7 @@ if [ $# -gt 3 ]; then
 else
   exclude_s_pattern=' '
 fi
-s_pattern='*.csv'
+
 p_pattern='{printf "%30s, %40s, %40s, %40s, %40s\n", $1,$2,$5,$6,$8}'
-find  $RESULTS_DIR -iname "$s_pattern" | grep -v "$exclude_s_pattern" | grep -m 1 '.csv' |xargs head -n 1 |awk -F ',' "$p_pattern"
+find  $RESULTS_DIR -iname "$s_pattern" | grep -v "$exclude_s_pattern" | sed 's/ /\\ /g'  | head -n 1 |xargs head -n 1 |awk -F ',' "$p_pattern"
 find  $RESULTS_DIR -iname "$s_pattern" | grep -v "$exclude_s_pattern" | sed 's/ /\\ /g'  | xargs awk '{print $0}' |grep  'Learner' -v |sed "s#ArffFileStream -f ##g" |sed "s#${DATASET_DIR}##g" |sed "s#/\/#\\\#g" |sed 's/.arff//g' | awk -F ',' "$p_pattern"
