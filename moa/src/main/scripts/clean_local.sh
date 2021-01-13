72aef7825553fb3576097246fec2eeea4c91f339
Nuwan Gunasekara <ng198002@gmail.com>
2021-01-13 23:00:06 +1300

    Add more directories to clean

--- moa/src/main/scripts/clean_local.sh
+++ moa/src/main/scripts/clean_local.sh
@@ -5,3 +5,6 @@ rm -r ~/.m2/repository/com/google/code/gson
 rm -r ~/.m2/repository/net/java/dev/jna 
 rm -r ~/.m2/repository/org/apache/commons
 rm -r ~/.djl.ai
+rm -r ~/Desktop/m2_cache/* ~/Desktop/djl.ai/*
+rm -r /Scratch/ng98/JavaSetup1/djl.ai/* /Scratch/ng98/JavaSetup1/local_m2/*
+
