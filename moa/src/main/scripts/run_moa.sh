25fa0ce3d2b5fa93c9076284c12ae6086b01ef59
Nuwan Gunasekara <ng198002@gmail.com>
2021-01-13 20:04:27 +1300

    Kill all child processors

--- moa/src/main/scripts/run_moa.sh
+++ moa/src/main/scripts/run_moa.sh
@@ -67,7 +67,7 @@ case $(uname)  in
     ;;
 esac
 
-# learner='meta.StreamingRandomPatches -s 10'
+#learner='meta.StreamingRandomPatches -s 10'
 learner='neuralNetworks.MultiMLP -h -n -t UseThreads'
 # learner='lazy.kNN'
 
@@ -133,7 +133,44 @@ time "$JCMD" \
         echo -ne "Waiting for exp with $PID to finish\r"
       fi
     done
+    echo "Child processors============================="
+    #Store the current Process ID, we don't want to kill the current executing process id
+    CURPID=$$
 
+    # This is process id, parameter passed by user
+    ppid=$PID
+
+    if [ -z $ppid ] ; then
+       echo No PID given.
+    fi
+
+    arraycounter=1
+    while true
+    do
+            FORLOOP=FALSE
+            # Get all the child process id
+            for i in `ps -ef| awk '$3 == '$ppid' { print $2 }'`
+            do
+                    if [ $i -ne $CURPID ] ; then
+                            procid[$arraycounter]=$i
+                            arraycounter=`expr $arraycounter + 1`
+                            ppid=$i
+                            FORLOOP=TRUE
+                    fi
+            done
+            if [ "$FORLOOP" = "FALSE" ] ; then
+               arraycounter=`expr $arraycounter - 1`
+               ## We want to kill child process id first and then parent id's
+               while [ $arraycounter -ne 0 ]
+               do
+                 echo "killing ${procid[$arraycounter]}"
+                 kill -9 "${procid[$arraycounter]}" >/dev/null
+                 arraycounter=`expr $arraycounter - 1`
+               done
+             break
+            fi
+    done
+    echo "Child processors============================="
     kill $PID
   fi
 
