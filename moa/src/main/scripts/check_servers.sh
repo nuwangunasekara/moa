112c01941f221b9d9dce05c9719232127ce9fb1a
Nuwan Gunasekara <ng198002@gmail.com>
2021-01-18 23:45:11 +1300

    Add support to print info of one server

--- moa/src/main/scripts/check_servers.sh
+++ moa/src/main/scripts/check_servers.sh
@@ -3,15 +3,10 @@
 command='echo -e "HOST=$(hostname).$(dnsdomainname) \t CPU_COUNT=$(( $(lscpu -e=CPU |tail -n1) + 1 )) \t TOP=$(ps h -Ao user,comm,pcpu --sort=-pcpu |head -n 1) \t NO_OF_USERS=$(who |wc -l) \t MEM=$(free -h | grep -i 'Mem:')"'
 
 gpu_query_gpu_command='nvidia-smi --query-gpu=gpu_name,pci.bus_id,utilization.gpu,utilization.memory,memory.total,memory.free --format=csv'
+gpu_get_driver='nvidia-smi'
+gpu_get_cuda_version='nvcc --version'
 gpu_query_compute_apps_command='nvidia-smi --query-compute-apps=gpu_bus_id,pid,used_gpu_memory,process_name --format=csv'
 gpu_query_accounted_apps_command='nvidia-smi --query-accounted-apps=gpu_bus_id,pid,gpu_utilization,mem_utilization,max_memory_usage  --format=csv'
-
-if [ $# -gt 0 ]; then
-  if [ "$1" == "local" ]; then
-    print_server_info "$1" show_gpu
-    exit 0
-  fi
-fi
 
 domain_name='cms.waikato.ac.nz'
 
@@ -30,6 +25,10 @@ print_server_info()
   if [ $# -gt 1 ]; then
     if [ "$2" == "show_gpu" ]; then
       echo "------------------------------------------------------------------------------------------------------------------------"
+      ${ssh_command}  "$gpu_get_driver"
+      echo "........................................................................................................................"
+      ${ssh_command}  "$gpu_get_cuda_version"
+      echo "........................................................................................................................"
       ${ssh_command}  "$gpu_query_gpu_command" | awk -F ',' '{printf "%20s %20s %20s %20s %20s %20s\n", $1,$2,$3,$4,$5,$6}'
       echo "........................................................................................................................"
       ${ssh_command}  "$gpu_query_compute_apps_command" | awk -F ',' '{printf "%20s %20s %20s %20s %40s\n", " ",$1,$2,$3,$4}'
@@ -39,6 +38,16 @@ print_server_info()
     fi
   fi
 }
+
+if [ $# -gt 0 ]; then
+  if [ "$1" == "local" ]; then
+    print_server_info "$1" show_gpu
+    exit 0
+  else
+    print_server_info "$1" show_gpu
+    exit 0
+  fi
+fi
 
 echo -e "\nServers with up to one GPU, all with 64 CPU RAM:\n"
 for i in duet quartet quatern;
