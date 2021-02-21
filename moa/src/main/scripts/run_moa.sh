29d3e2665257ed3a3372502a40aa21758ab8944c
Nuwan Gunasekara <ng198002@gmail.com>
2021-02-21 23:55:04 +1300

    Add more hi-dem datasets Support for randam seed on SRP and ARF

--- moa/src/main/scripts/run_moa.sh
+++ moa/src/main/scripts/run_moa.sh
@@ -14,17 +14,19 @@ echo "Script pid = $SCRIPT_PID"
 
 #dataset=(spam_corpus WISDM_ar_v1.1_transformed elecNormNew nomao covtypeNorm kdd99 airlines RBF_f RBF_m LED_g LED_a AGR_a AGR_g)
 dataset=(RBF_f RBF_m LED_g LED_a AGR_a AGR_g spam_corpus kdd99 airlines WISDM_ar_v1.1_transformed elecNormNew nomao covtypeNorm)
-dataset=(real-sim.libsvm.class_Nominal_sparse SVHN.scale.t.libsvm.sparse_class_Nominal sector.scale.libsvm.class_Nominal_sparse)
+dataset=(RBF_f RBF_m LED_g LED_a AGR_a AGR_g spam_corpus kdd99 airlines WISDM_ar_v1.1_transformed elecNormNew nomao covtypeNorm real-sim.libsvm.class_Nominal_sparse SVHN.scale.t.libsvm.sparse_class_Nominal sector.scale.libsvm.class_Nominal_sparse rcv1_train.binary_class_Nominal rcv1_train.multiclass_class_Nominal.arff gisette_scale_class_Nominal epsilon_normalized.t_class_Nominal)
 
 datasets_to_repeat=(WISDM_ar_v1.1_transformed elecNormNew nomao)
 max_repeat=0
+
+random_seed=9
 
 # times to re-run on failure
 max_re_run_count=0
 
 learners=('neuralNetworks.MultiMLP -h -n -t UseThreads -o 2 -O 8 -N 9' 'meta.StreamingRandomPatches1 -s 10' 'meta.AdaptiveRandomForest1 -s 10 -j 10' )
 learners=('neuralNetworks.MultiMLP -h -n -t UseThreads -o 8 -O 8 -N 9 -R' 'neuralNetworks.MultiMLP -h -n -t UseThreads -o 8 -O 8 -N 9' 'meta.StreamingRandomPatches1 -s 10' 'meta.AdaptiveRandomForest1 -s 10 -j 10' 'neuralNetworks.MultiMLP -h -n -t UseThreads -o 2 -O 8 -N 9' 'neuralNetworks.MultiMLP -h -n -t UseThreads -o 2 -O 8 -N 11')
-learners=('meta.StreamingRandomPatches1 -s 10' 'neuralNetworks.MultiMLP -h -n -t UseThreads -o 8 -O 8 -N 9 -b 0.0' 'neuralNetworks.MultiMLP -h -n -t UseThreads -o 8 -O 8 -N 9 -b 0.6')
+learners=('meta.AdaptiveRandomForest1 -s 10 -j 10' 'meta.StreamingRandomPatches1 -s 10' 'neuralNetworks.MultiMLP -h -n -t UseThreads -o 8 -O 8 -N 9 -b 0.0' 'neuralNetworks.MultiMLP -h -n -t UseThreads -o 8 -O 8 -N 9 -b 0.6')
 
 sample_frequency=1000000
 use_10_percent_sample_frequency=0
@@ -149,7 +151,11 @@ do
     if [[ "$learner" =~  "MultiMLP" ]]; then
       learner_command="$learner -s $warmup_instances"
     else
-      learner_command="$learner"
+      if [[ "$learner" =~  "AdaptiveRandomForest1" ]] || [[ "$learner" =~  "StreamingRandomPatches1" ]]; then
+        learner_command="$learner -r $random_seed"
+      else
+        learner_command="$learner"
+      fi
     fi
 
     if [ -f $out_file ]; then
